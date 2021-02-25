34dd6c54f3d812234bd790bf8e66d967da572ba5
Максим <41415269+KemtaTam@users.noreply.github.com>
2021-02-26 00:42:00 +0700

    Update script.js

--- script.js
+++ script.js
@@ -26,27 +26,49 @@ const {form} = document.forms;
 	  
 	  //---------------УДАЛЕНИЕ ранее созданной таблицы----------------
 	  let t = tbody.rows.length;
-	  for(i=0; i<t; i++){
+	  for(i=0; i<t; i++) {
 		  tbody.deleteRow(0);
 	  }
 	 
-      let   tr1 = document.createElement("tr");
+	  //первая строка
+      let tr1 = document.createElement("tr");
       tr1.setAttribute("class", "first_row");
       tbody.appendChild(tr1);
-  
+	  //первая ячейка первой строки
       let th1 = document.createElement("th");
       th1.setAttribute("class", "first_col t_1-1");
       th1.textContent = "I/S";
-      tr1.appendChild(th1);
-  
+	  tr1.appendChild(th1);
+
+	  //создание всех ячеек первой строки
+	  for(i=0; i<form.input_states.value; i++)
+	  {
+		  let th = document.createElement("th");
+		  th.textContent = i+1;
+		  tr1.appendChild(th);
+	  }
+	  //создание остальных строк
 	  for (i = 0; i < form.input_characters.value; i++) 
 	  {
         let tr = document.createElement("tr");
-        tr.textContent = String.fromCharCode(97 + i);
         tbody.appendChild(tr);
-  
-		
 
+		let th1 = document.createElement("th");
+		th1.style.background =  "#F8CBADFF";
+		th1.textContent = String.fromCharCode(97 + i);
+		tr.appendChild(th1);
+
+        for(j=0; j<form.input_states.value; j++)
+        {
+			let th = document.createElement("th");
+			let input_value = document.createElement("input");
+			//добавить анимацию?
+			input_value.setAttribute('class', 'valueOfTable');
+			input_value.type = "text";
+			input_value.placeholder = '0/0';
+			th.appendChild(input_value);
+			tr.appendChild(th);
+		}
       }
   };
   enter_button.onclick = CreateTable;
@@ -54,7 +76,7 @@ const {form} = document.forms;
   
 
   function checkForm(form){
-	var e = 0;
+	let e = 0;
 	for (var i = 0; i < form.length - 1; i++) 
 	{
 		if (!form[i].value.replace(/^\s+|\s+$/g, "")) 
